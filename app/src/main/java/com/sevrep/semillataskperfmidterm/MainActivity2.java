ed1a51ae71caf19e728879612dc8620268446a55
Ivan <sevrep@yahoo.com>
2020-11-14 18:18:52 +0800

    Done

--- app/src/main/java/com/sevrep/semillataskperfmidterm/MainActivity2.java
+++ app/src/main/java/com/sevrep/semillataskperfmidterm/MainActivity2.java
@@ -33,7 +33,6 @@ public class MainActivity2 extends AppCompatActivity implements BottomNavigation
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
